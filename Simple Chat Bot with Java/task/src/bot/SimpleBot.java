90edec5763ae8af8e4f39ac0495468c840d20cdc
Gafe <gabriel875@yahoo.com.br>
2026-01-25 06:45:25 -0300

    Project Stage 1: Houston is Born

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -3,5 +3,12 @@ package bot;
 public class SimpleBot {
     public static void main(String[] args) {
         // write your code here
+
+        String botName = "Houston";
+        int birthYear = 2026;
+
+        System.out.println("Hello! My name is " + botName);
+        System.out.println("I was created in " + birthYear + ".");
+
     }
 }
